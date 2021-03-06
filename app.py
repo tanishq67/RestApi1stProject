ab8e53cb950177dda0fe8d2f1e8f281ec96a9f99
tanishq67 <tanishq67@gmail.com>
2021-03-06 23:03:45 +0530

    Added a framework and basics

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+class HelloWorld(Resource):
+    def get(self, name, test):
+        return {'name': name, 'test': test}
+
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
